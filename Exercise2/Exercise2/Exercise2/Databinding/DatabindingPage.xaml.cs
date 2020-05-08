aef7e5d415f78fb7dab8bfeca2a1d74e60c0eb0e
sabapathygithub <37434203+sabapathygithub@users.noreply.github.com>
2020-05-08 12:03:10 +0530

    Second exercise is added to learning repo
    This sample projects holds example for the following Xamarin.Forms concept.
    - Master Details Page & Content Page
    - Layouts (Stack, Grid, Absolute, Relative)
    - Databindings
    - Styles.

---
+++ Exercise2/Exercise2/Exercise2/Databinding/DatabindingPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exercise2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatabindingPage : ContentPage
+    {
+        string[] quotes = new string[3]
+       {
+            "I measure the progress of a community by the degree of progress which women have achieved.",
+            "You cannot expect any rational thought from a religious man. He is like a rocking log in water.",
+            "The relationship between husband and wife should be one of closest friends."
+       };
+
+        int currentQuote = 0;
+
+        public DatabindingPage()
+        {
+            InitializeComponent();
+            DisplayQuote();
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            DisplayQuote();
+        }
+
+        private void DisplayQuote()
+        {
+            QuoteLabel.Text = quotes[currentQuote];
+            currentQuote++;
+            if (currentQuote >= quotes.Length)
+                currentQuote = 0;
+        }
+    }
+}
---
+++ Exercise2/Exercise2/Exercise2/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exercise2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MenuList.ItemSelected += MenuList_ItemSelected;
+        }
+
+        private void MenuList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenu = e.SelectedItem as MasterPageItem;
+            if(selectedMenu != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(selectedMenu.TargetType));
+                Detail.Title = selectedMenu.Title;
+                MenuList.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+
+    public class MasterPageItem
+    {
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
